7b51492cd4ff63d78980f72fb5b66bc85506ff56
ChebiZx47 <haris.milk41@bk.ru>
2024-12-19 23:39:48 +0300

    Добавлена функция ввода и вывода суммы

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int a,b;
+    std::cout<<"введите число a"<<std::endl;
+    std::cin>>a;
+    std::cout<<"введите число b"<<std::endl;
+    std::cin>>b;
+    int sum = a+b;
+
+    std::cout<<"Сумма чисел : "<<sum<<std::endl;
+}
